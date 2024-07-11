bddd07e77a3b4974c639c4580c0b9572ee78031e
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-07-11 13:34:00 +0800

    interval removed, await for state machine implementation

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,6 +1,4 @@
 import { getStatus } from '../functions/actions';
-import move_left from '../images/move/move_left.gif';
-import research_left from '../images/research/research_left.gif';
 import { Status } from '../models';
 import { useState, useEffect } from 'react';
 import './Character.css';
@@ -8,16 +6,24 @@ import { standLeft } from "../contants";
 
 export default function Character(): React.JSX.Element {
     const [status, setStatus] = useState<Status>(standLeft);
+    const [position, setPosition] = useState<number>(0);
 
     useEffect(() => {
-        setTimeout(()=>{
+        setTimeout(() => {
             const newStatus = getStatus(status);
             setStatus(newStatus);
+            // const intervalId = setInterval(() => {
+            //     if (newStatus.speed){
+            //         const newPosition = position + newStatus.speed;
+            //         setPosition(newPosition);
+            //     }
+            // }, newStatus.duration / 10)
         }, status.duration)
     }, [status])
 
     return (
-        <div>
-            <img src={status.src} className={"character"}></img>
+        <div className={"container"}>
+            {/* dock length: 100px - 1250px */}
+            <img src={status.src} className={"character"} alt={"loading"} style={{ left: 700 + position }}></img>
         </div>);
 }
